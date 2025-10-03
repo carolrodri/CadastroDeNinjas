9b0c8d2ae880893a83e00bad831a2e083842917c
Caroline Rodrigues <caroline101910@hotmail.com>
2025-10-03 15:19:03 -0300

    Criado a controller-aula2

--- src/main/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -1,4 +1,4 @@
-package dev.carol.CadastroDeNinjas;
+package CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
