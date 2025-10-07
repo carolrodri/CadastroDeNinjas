55ea866962a7222bea85e5bb0e7da496ea8f6558
Caroline Rodrigues <caroline101910@hotmail.com>
2025-10-07 14:30:22 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no Ninja Mapper os caminhos entidades entidade x dto

--- src/main/java/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,24 @@
 package CadastroDeNinjas.Ninjas;
 
+import CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    private MissoesModel missoes;
+
 }
