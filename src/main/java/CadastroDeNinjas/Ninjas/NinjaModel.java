a9f17a88bb5fa00c23a25bb284ff00791b9065a7
Caroline Rodrigues <caroline101910@hotmail.com>
2025-10-07 17:16:29 -0300

    Inclui a annotation ToString nos models de Ninja e Missoes

--- src/main/java/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Data //Cria todos os getters and setters (fica "invisivel")
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 public class NinjaModel {
 
     @Id
@@ -35,7 +37,6 @@ public class NinjaModel {
 
     @Column(name = "idade")
     private int idade;
-
 
 
     //@ManyToone um ninja tem uma unica missao
